9ad4dd53cfdefb3242e4f1d6a8cf69a2c55aeafc
Joe Murphy <jmurphy@denverpost.com>
2012-12-14 18:15:02 -0700

    start writing the compile script

--- deploy/compile.sh
+++ deploy/compile.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
 # Script to convert all the .txt files to .js arrays
+
+#cd ../lists/denverpost/
+cd /home/joe/work/heads/lists/denverpost/
+
+for i in `ls *.txt`; 
+do 
+  # Get the first part of the file name
+  slug=(`echo $i | tr '.' ' '`)
+  echo $slug "= array(" > $slug.js
+  for line in `cat $i`; do
+    echo "\"$line\"," >> $slug.js
+  done
+  echo ");" >> $slug.js
+done
